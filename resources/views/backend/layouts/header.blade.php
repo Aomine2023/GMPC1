85eb6cb87daaa7df60f1eadd67e92ef87fcf0d67
Isaac Totimeh <87771826+GreatLogi@users.noreply.github.com>
2025-01-04 09:34:18 +0000

    Updated the header of the page

--- resources/views/backend/layouts/header.blade.php
+++ resources/views/backend/layouts/header.blade.php
@@ -57,13 +57,6 @@
                     <div class="dropdown-menu dropdown-menu-end">
                         <!-- item-->
                         <h6 class="dropdown-header">{{ Auth::user()->name }}</h6>
-                        <a class="dropdown-item" href="pages-profile.html"><i
-                                class="mdi mdi-account-circle text-muted fs-16 align-middle me-1"></i> <span
-                                class="align-middle">Profile</span></a>
-                        <a class="dropdown-item" href="apps-chat.html"><i
-                                class="mdi mdi-message-text-outline text-muted fs-16 align-middle me-1"></i>
-                            <span class="align-middle">Messages</span></a>
-
                         <a class="dropdown-item" href="{{ route('logout') }}"><i
                                 class="mdi mdi-logout text-muted fs-16 align-middle me-1"></i> <span
                                 class="align-middle" data-key="t-logout">Logout</span></a>
