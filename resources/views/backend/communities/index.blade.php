677aa3ecf6e469767eb8179510d9fd8cbb5c6861
Legendbwoy <90243075+Legendbwoy@users.noreply.github.com>
2025-02-27 14:47:35 +0000

    added image table to communities

--- resources/views/backend/communities/index.blade.php
+++ resources/views/backend/communities/index.blade.php
@@ -1,59 +1,69 @@
 @extends('backend.layouts.master')
+
 @section('content')
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="card">
-                <div class="card-header">
-                    <h5 class="card-title mb-0">Communities</h5>
-                    <a href="{{ route('communities-add') }}" type="button" class="btn btn-primary waves-effect waves-light"
-                        style="float: right;">Add Community</a>
-                </div>
-                <div class="card-body">
-                    <table id="example" class="table table-bordered dt-responsive nowrap table-striped align-middle"
-                        style="width:100%">
-                        <thead>
+<div class="row">
+    <div class="col-lg-12">
+        <div class="card">
+            <div class="card-header">
+                <h5 class="card-title mb-0">Communities</h5>
+                <a href="{{ route('communities-add') }}" type="button" class="btn btn-primary waves-effect waves-light"
+                   style="float: right;">Add Community</a>
+            </div>
+            <div class="card-body">
+                <table id="example" class="table table-bordered dt-responsive nowrap table-striped align-middle"
+                       style="width:100%">
+                    <thead>
+                        <tr>
+                            <th data-ordering="false">SR No.</th>
+                            <th data-ordering="false">Name</th>
+                            <th data-ordering="false" width="60%">Body</th>
+                            <th data-ordering="false">Image</th> <!-- New Image Column -->
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($communities as $list)
                             <tr>
-                                <th data-ordering="false">SR No.</th>
-                                <th data-ordering="false">Name</th>
-                                <th data-ordering="false" width="60%">Body</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($communities as $list)
-                                <tr>
-                                    <td>{{ $loop->index + 1 }}</td>
-                                    <td>{{ $list->title ?? '' }}</td>
-                                    <td>{{ $list->body ?? '' }}</td>
-                                    <td>
-                                        <div class="dropdown d-inline-block">
-                                            <button class="btn btn-soft-secondary btn-sm dropdown" type="button"
+                                <td>{{ $loop->index + 1 }}</td>
+                                <td>{{ $list->title ?? '' }}</td>
+                                <td>{{ $list->body ?? '' }}</td>
+                                <td>
+                                    @if($list->image)
+                                        <img src="{{ asset('storage/' . $list->image) }}" alt="{{ $list->title }}" style="width: 100px; height: auto;">
+                                    @else
+                                        No Image
+                                    @endif
+                                </td>
+                                <td>
+                                    <div class="dropdown d-inline-block">
+                                        <button class="btn btn-soft-secondary btn-sm dropdown" type="button"
                                                 data-bs-toggle="dropdown" aria-expanded="false">
-                                                <i class="ri-more-fill align-middle"></i>
-                                            </button>
-                                            <ul class="dropdown-menu dropdown-menu-end">
-                                                <li>
-                                                    <a class="dropdown-item edit-item-btn"
-                                                        href="{{ route('communities-edit', $list->uuid) }}"><i
-                                                            class="ri-pencil-fill align-bottom me-2 text-muted"></i>
-                                                        Edit</a>
-                                                </li>
-                                                <li>
-                                                    <a class="dropdown-item remove-item-btn"
-                                                        href="{{ route('communities-delete', $list->uuid) }}">
-                                                        <i class="ri-delete-bin-fill align-bottom me-2 text-muted"></i>
-                                                        Delete
-                                                    </a>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
+                                            <i class="ri-more-fill align-middle"></i>
+                                        </button>
+                                        <ul class="dropdown-menu dropdown-menu-end">
+                                            <li>
+                                                <a class="dropdown-item edit-item-btn"
+                                                   href="{{ route('communities-edit', $list->uuid) }}">
+                                                   <i class="ri-pencil-fill align-bottom me-2 text-muted"></i>
+                                                   Edit
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a class="dropdown-item remove-item-btn"
+                                                   href="{{ route('communities-delete', $list->uuid) }}">
+                                                   <i class="ri-delete-bin-fill align-bottom me-2 text-muted"></i>
+                                                   Delete
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
+</div>
 @endsection
